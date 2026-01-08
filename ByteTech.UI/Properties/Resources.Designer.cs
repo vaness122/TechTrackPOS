45ebbcdebfcda112e557b09f1df1b0469f7f3d6b
Charlie <Charlie@DESKTOP-MS3T4S8>
2026-01-08 15:46:58 +0800

    "mock design of work station (NOT FINAL)"

--- ByteTech.UI/Properties/Resources.Designer.cs
+++ ByteTech.UI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace ByteTech.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap packages {
+            get {
+                object obj = ResourceManager.GetObject("packages", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
