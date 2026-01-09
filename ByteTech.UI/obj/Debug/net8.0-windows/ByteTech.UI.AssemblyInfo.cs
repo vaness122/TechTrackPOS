81b63b3423c374411e99b263ca4e4f59c7c29511
Charlie <Charlie@DESKTOP-MS3T4S8>
2026-01-09 12:38:00 +0800

    "updated database"

--- ByteTech.UI/obj/Debug/net8.0-windows/ByteTech.UI.AssemblyInfo.cs
+++ ByteTech.UI/obj/Debug/net8.0-windows/ByteTech.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ByteTech.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989d88b9f707e09e7ffc1de84d1e5f903045134d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ad5db4e796d5c8bdec61fcdfb939b3ff1bb06c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ByteTech.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ByteTech.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
