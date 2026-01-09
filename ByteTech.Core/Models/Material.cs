21ad5db4e796d5c8bdec61fcdfb939b3ff1bb06c
Charlie <Charlie@DESKTOP-MS3T4S8>
2026-01-09 12:32:05 +0800

    "added a few important fields for the product,    added location and inventory models for location specific   inventory of products"

---
+++ ByteTech.Core/Models/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteTech.Core.Models
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //plastic , organic etc
+        [StringLength(200)]
+        public string Description { get; set; }
+    }
+}
---
+++ ByteTech.UI/Admin_Dashboard.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace ByteTech.UI.Dashboard
+{
+    partial class Admin_Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin_Dashboard));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            panel4 = new Panel();
+            pictureBox2 = new PictureBox();
+            label4 = new Label();
+            panel5 = new Panel();
+            pictureBox4 = new PictureBox();
+            label5 = new Label();
+            panel6 = new Panel();
+            pictureBox5 = new PictureBox();
+            label6 = new Label();
+            panel7 = new Panel();
+            panel10 = new Panel();
+            label9 = new Label();
+            panel9 = new Panel();
+            label8 = new Label();
+            panel8 = new Panel();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel7.SuspendLayout();
+            panel10.SuspendLayout();
+            panel9.SuspendLayout();
+            panel8.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(999, 28);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(3, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Work Station";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(12, 45);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(119, 85);
+            panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(36, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Stock";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.packages;
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(36, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(36, 37);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonHighlight;
+            panel3.Controls.Add(pictureBox3);
+            panel3.Controls.Add(label3);
+            panel3.Location = new Point(137, 45);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(119, 85);
+            panel3.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.InitialImage = null;
+            pictureBox3.Location = new Point(43, 13);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(36, 37);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(34, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Payment";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ButtonHighlight;
+            panel4.Controls.Add(pictureBox2);
+            panel4.Controls.Add(label4);
+            panel4.Location = new Point(262, 45);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(119, 85);
+            panel4.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.InitialImage = null;
+            pictureBox2.Location = new Point(39, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(36, 37);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(36, 51);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Invoice";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.ButtonHighlight;
+            panel5.Controls.Add(pictureBox4);
+            panel5.Controls.Add(label5);
+            panel5.Location = new Point(387, 45);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(119, 85);
+            panel5.TabIndex = 4;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.InitialImage = null;
+            pictureBox4.Location = new Point(34, 8);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(36, 37);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(24, 48);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Customers";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonHighlight;
+            panel6.Controls.Add(pictureBox5);
+            panel6.Controls.Add(label6);
+            panel6.Location = new Point(512, 45);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(119, 85);
+            panel6.TabIndex = 2;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.InitialImage = null;
+            pictureBox5.Location = new Point(39, 13);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(36, 37);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(15, 50);
+            label6.Name = "label6";
+            label6.Size = new Size(95, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Business Details";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.AppWorkspace;
+            panel7.Controls.Add(panel10);
+            panel7.Controls.Add(panel9);
+            panel7.Controls.Add(panel8);
+            panel7.Location = new Point(809, 27);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(190, 521);
+            panel7.TabIndex = 5;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = SystemColors.ButtonHighlight;
+            panel10.Controls.Add(label9);
+            panel10.Location = new Point(20, 224);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(159, 85);
+            panel10.TabIndex = 8;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(3, 11);
+            label9.Name = "label9";
+            label9.Size = new Size(110, 15);
+            label9.TabIndex = 5;
+            label9.Text = "Total Sales (Credit)";
+            // 
+            // panel9
+            // 
+            panel9.BackColor = SystemColors.ButtonHighlight;
+            panel9.Controls.Add(label8);
+            panel9.Location = new Point(20, 133);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(156, 85);
+            panel9.TabIndex = 7;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(3, 11);
+            label8.Name = "label8";
+            label8.Size = new Size(101, 15);
+            label8.TabIndex = 5;
+            label8.Text = "Total Sales (Cash)";
+            // 
+            // panel8
+            // 
+            panel8.BackColor = SystemColors.ButtonHighlight;
+            panel8.Controls.Add(label7);
+            panel8.Location = new Point(20, 42);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(156, 85);
+            panel8.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(3, 11);
+            label7.Name = "label7";
+            label7.Size = new Size(108, 15);
+            label7.TabIndex = 5;
+            label7.Text = "Total Sales (Today)";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OliveDrab;
+            button1.Location = new Point(637, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 77);
+            button1.TabIndex = 9;
+            button1.Text = "Refresh";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.IndianRed;
+            button2.Location = new Point(718, 49);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 77);
+            button2.TabIndex = 10;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Admin_Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Admin_Dashboard";
+            Size = new Size(999, 548);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel7.ResumeLayout(false);
+            panel10.ResumeLayout(false);
+            panel10.PerformLayout();
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Panel panel3;
+        private Label label3;
+        private Panel panel4;
+        private Label label4;
+        private Panel panel5;
+        private Panel panel6;
+        private Label label5;
+        private Label label6;
+        private Panel panel7;
+        private Panel panel10;
+        private Label label9;
+        private Panel panel9;
+        private Label label8;
+        private Panel panel8;
+        private Label label7;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Button button1;
+        private Button button2;
+    }
+}
